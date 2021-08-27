bfd263770b42f4d6c11c71a3145080aecb1ba2b7
jayspm <53whiteroad@gmail.com>
2021-08-27 15:00:23 +1000

    car controller script updated

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
 
 public class CarController : MonoBehaviour
 {
+    /*  ----- CarController Class -----
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
     private float horizontalInput;
     private float verticalInput;
     private float steerAngle;
@@ -21,15 +32,22 @@ public class CarController : MonoBehaviour
     public float maxSteeringAngle = 30f;
     public float motorForce = 50f;
     public float brakeForce = 0f;
+    
+    public float speed, turningSpeed;
+    Vector3 direction;
+    Quaternion rotation;
+    Rigidbody carRigibody;
 
-    // for car flipping issue fix
-    // to move center of mass up
-    // public float mass = -0.9f;   for normal in unity
+    /* ----- For fixing car flipping issue -----
+     * Fixed by moving center of the mass up
+     * public float mass = -0.9f;   >> for normal in unity
+     */
     public float mass = 0f;
 
     void Start()
     {
-        GetComponent<Rigidbody>().centerOfMass = new Vector3(0f, mass, 0f);
+        carRigibody = GetComponent<Rigidbody>();
+        carRigibody.centerOfMass = new Vector3(0f, mass, 0f);
     }
 
     private void FixedUpdate()
@@ -42,9 +60,26 @@ public class CarController : MonoBehaviour
 
     private void GetInput()
     {
+        /* ----- Car movement -----
+         * Horizontal is to move left and right
+         * Vertical is to move forward and back
+         */
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         isBreaking = Input.GetKey(KeyCode.Space);
+
+        var primaryInput = VRDevice.Device.PrimaryInputDevice;
+        var inputs = new Vector2(horizontalInput, verticalInput); 
+        var trigger = primaryInput.GetAxis1D(VRButton.Trigger);
+        isBreaking = Input.GetKey(KeyCode.Space);
+
+        if(trigger > 0.01 || isBreaking)
+        {
+            direction.Set(inputs.x, 0, inputs.y);
+            rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, turningSpeed);
+            carRigibody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+        }
     }
 
     private void HandleSteering()
