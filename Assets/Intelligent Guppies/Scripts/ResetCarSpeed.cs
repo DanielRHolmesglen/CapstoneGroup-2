8da26010af2693cf3805e5b081fb523ab3ef7440
jayspm <53whiteroad@gmail.com>
2021-09-10 11:56:20 +1000

    resetCarSpeed script is done

--- Assets/Intelligent Guppies/Scripts/ResetCarSpeed.cs
+++ Assets/Intelligent Guppies/Scripts/ResetCarSpeed.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class ResetCarSpeed : MonoBehaviour
 {
-
+    private float speedChanged = 500f;
+    private float carCurrentSpeed;
     void Start()
     {
         GetComponent<BoxCollider>().isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider _other)
     {
-        
+        if (_other.gameObject)
+        {
+            ResetSpeed(_other);
+            gameObject.SetActive(false);
+        }
+    }
+
+    void ResetSpeed(Collider _car)
+    {
+        CarController carControllerAccessd = _car.GetComponent<CarController>();
+        carCurrentSpeed = carControllerAccessd.motorForce;
+        Debug.Log("Current speed = " + carCurrentSpeed);
+
+        if (carCurrentSpeed > 300)
+        {
+            carCurrentSpeed -= speedChanged;
+            Debug.Log("current new speed = " + carCurrentSpeed);
+            carControllerAccessd.motorForce = carCurrentSpeed;
+        }
+        else if (carCurrentSpeed < 300)
+        {
+            carCurrentSpeed += speedChanged;
+            Debug.Log("current new speed = " + carCurrentSpeed);
+            carControllerAccessd.motorForce = carCurrentSpeed;
+        }
     }
 }
